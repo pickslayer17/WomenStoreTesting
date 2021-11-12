ffe6ce61913139e894d733b54f962ef51f87a370
pickslayer17 <dzianis.piatrovich@gmail.com>
2021-11-12 16:00:10 +0300

    add wait for button

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -14,6 +14,7 @@ public class HomePage extends AbstractPage {
     }
 
     public void clickSignInButton() {
+        waitUntilElement_IsClickable(signInButton, 10);
         signInButton.click();
     }
 }

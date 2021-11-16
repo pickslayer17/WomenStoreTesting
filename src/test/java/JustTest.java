5f3112921ff1dfa79e84ef2aa38e7a8d3ad2617e
pickslayer17 <dzianis.piatrovich@gmail.com>
2021-11-16 20:53:27 +0300

    just test

---
+++ src/test/java/JustTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+
+public class JustTest {
+
+
+    @Test
+    public void testingTest(){
+        System.out.println();
+        System.out.println();
+        System.out.println("Oh my GOD!!!!!");
+        System.out.println();
+        System.out.println();
+    }
+}

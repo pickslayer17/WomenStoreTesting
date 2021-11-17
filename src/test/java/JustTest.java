667dc6a0f7c2e09896f4baba567cc9263f4ac81e
pickslayer17 <dzianis.piatrovich@gmail.com>
2021-11-17 14:42:32 +0300

    it works

--- src/test/java/JustTest.java
+++ src/test/java/JustTest.java
@@ -1,10 +1,12 @@
-import io.qameta.allure.Feature;
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
 public class JustTest {
 
-    @Feature("Pither")
+
+
     @Test
     public void testingTest() {
         System.out.println();
@@ -12,5 +14,25 @@ public class JustTest {
         System.out.println("Oh my GOD!!!!!");
         System.out.println();
         System.out.println();
+        step1();
+    }
+    @Step
+    private void step1() {
+        System.out.println("Hi! im very tired step 2");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+
+    @Test
+    public void simpleTest2() {
+        checkSumStep(3, 2, 5);
+        checkSumStep(5, 4, 9);
+    }
+    @Step
+    public static void checkSumStep(int num1, int num2, int expectedSum) {
+        Assertions.assertTrue(num1 + num2 == expectedSum, "Сумма слагаемых не соответствует ожидаемому значению");
     }
 }
